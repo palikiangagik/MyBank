f808b1b24e8c890d6eb402714c146f9d5e9cca06
palikiangagik <palikiangagik@gmail.com>
2026-04-27 06:43:05 +0400

    Simplify init script for DB.

--- MyBank.Infrastructure/Migrations/Scripts/InitialSetup.sql
+++ MyBank.Infrastructure/Migrations/Scripts/InitialSetup.sql
@@ -3,22 +3,19 @@ BEGIN
     CREATE SEQUENCE dbo.IdSequence START WITH 1 INCREMENT BY 1;
 END
 
-
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Accounts')
 BEGIN
     CREATE TABLE Accounts (
         Id INT PRIMARY KEY,
         Code NVARCHAR(100) NOT NULL, 
-        UserId NVARCHAR(450) NOT NULL,
+        UserId NVARCHAR(450) NOT NULL REFERENCES AspNetUsers(Id),
         Balance DECIMAL(18,2) DEFAULT 0,
-        IsClosed BIT DEFAULT 0,
-        CONSTRAINT FK_Accounts_Users FOREIGN KEY (UserId) REFERENCES AspNetUsers(Id),
-
-        INDEX IX_Accounts_UserId (UserId)
+        IsClosed BIT DEFAULT 0
     );
     
+    CREATE INDEX IX_Accounts_UserId 
+    ON Accounts(UserId);
 END
-
 
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Transactions')
 BEGIN
@@ -27,23 +24,23 @@ BEGIN
         CreatedAt DATETIME NOT NULL,
         [Type] INT NOT NULL,
         Amount DECIMAL(18, 2) NOT NULL,
-        SenderAccountId INT NULL,
-        RecipientAccountId INT NULL,
-        AccountId INT NULL,
-
-        CONSTRAINT FK_Transactions_Sender FOREIGN KEY (SenderAccountId) 
-            REFERENCES Accounts(Id),
-            
-        CONSTRAINT FK_Transactions_Recipient FOREIGN KEY (RecipientAccountId) 
-            REFERENCES Accounts(Id),
-            
-        CONSTRAINT FK_Transactions_Account FOREIGN KEY (AccountId) 
-            REFERENCES Accounts(Id),
-
-        INDEX IX_Transactions_AccountId (AccountId DESC),
-        INDEX IX_Transactions_SenderAccountId (SenderAccountId) WHERE SenderAccountId IS NOT NULL,
-        INDEX IX_Transactions_RecipientAccountId (RecipientAccountId) WHERE RecipientAccountId IS NOT NULL,
-        INDEX IX_Transactions_CreatedAt (CreatedAt DESC)
+        SenderAccountId INT NULL REFERENCES Accounts(Id),
+        RecipientAccountId INT NULL REFERENCES Accounts(Id),
+        AccountId INT NULL REFERENCES Accounts(Id)
     );
+
+    CREATE INDEX IX_Transactions_AccountId 
+    ON Transactions (AccountId DESC);
+
+    CREATE INDEX IX_Transactions_CreatedAt 
+    ON Transactions (CreatedAt DESC);
+
+    CREATE INDEX IX_Transactions_SenderAccountId 
+    ON Transactions (SenderAccountId) 
+    WHERE SenderAccountId IS NOT NULL;
+
+    CREATE INDEX IX_Transactions_RecipientAccountId 
+    ON Transactions (RecipientAccountId) 
+    WHERE RecipientAccountId IS NOT NULL;
 END
 
